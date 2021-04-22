503a7cbeef9ffa9803a160898a40a0b452677d9d
jleonard17 <jl682@njit.edu>
2021-04-22 19:18:56 -0400

    commit on 'main'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import React from 'react';
-import {useState} from 'react'
+import React, { useEffect } from 'react';
+import {useState} from 'react';
+import MovieList from './components/MovieList'
+import MovieListHeading from './components/MovieListHeading';
+
+
 
 function App() {
   const [movies, setMovies] = useState([]) 
+  const [searchValue, setSearchValue] = useState("")
+
+  const getMovieRequest = async() => {
+    const url = 'http://www.omdbapi.com/?s=star wars&apikey=7e4d27ca'
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    console.log(responseJson)
+    setMovies(responseJson.Search)
+  }
+
+  useEffect(() => {
+    getMovieRequest()
+  }, [])
+
   return (
-    <App />
+    <div className = 'container-fluid movie-app'>
+      <div className = "row">
+        <MovieListHeading heading = 'movies'/>
+      </div>
+      <div className = 'row'>
+        <MovieList movies={movies}/>
+      </div>
+    </div>
   );
 }
 
 export default App;
+
---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchBar = () => {
+    return (
+        <div className = 'col col-sm-4'>
+            <input className = 'form-control'> type to search</input>
+        </div>
+    )
+}
+
+export default SearchBar
