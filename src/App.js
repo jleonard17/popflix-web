365e4024760977f281ccf82bb2f8b13c4822fb0f
jleonard17 <jl682@njit.edu>
2021-04-23 22:09:30 -0400

    add/remove watchlist

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import {useState} from 'react';
 import MovieList from './components/MovieList'
 import MovieListHeading from './components/MovieListHeading';
 import SearchBar from './components/SearchBar';
+import AddWatched from './components/AddWatched';
+import RemoveWatched from './components/RemoveWatched';
 
 
 
 function App() {
+
+
   const [movies, setMovies] = useState([]) 
   const [searchValue, setSearchValue] = useState(``)
+  const [watched, setWatched] = useState([])
+
+
 
   const getMovieRequest = async(searchValue) => {
     const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=7e4d27ca`;
@@ -23,9 +30,28 @@ function App() {
     }
   }
 
+  
   useEffect(() => {
     getMovieRequest(searchValue);
   }, [searchValue]);
+
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem('popFlix-movie-favorites', JSON.stringify(items))
+  }
+
+
+  const addWatchedMovie = (movie) => {
+    const newWatchedList = [...watched, movie]
+    setWatched(newWatchedList);
+    saveToLocalStorage(newWatchedList);
+  }
+
+
+  const removeWatchedMovie = (movie) => {
+    const newWatchedList = watched.filter((watched) => watched.imdbID !== movie.imdbID);
+    setWatched(newWatchedList);
+  }
+
 
   return (
     <div className = 'container-fluid movie-app'>
@@ -33,9 +59,25 @@ function App() {
         <MovieListHeading heading = 'PopFlix'/>
         <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
+
+      
       <div className = 'row'>
-        <MovieList movies={movies}/>
+        <MovieList movies={movies} 
+        watchedComponent={AddWatched} 
+        handleWatchedClick = {addWatchedMovie}/>
       </div>
+
+      <div className = "row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading = 'Watched List'/>
+      </div>
+
+      <div className = 'row'>
+        <MovieList movies={watched} 
+        watchedComponent={RemoveWatched} 
+        handleWatchedClick = {removeWatchedMovie}/>
+      </div>
+
+
     </div>
   );
 }
